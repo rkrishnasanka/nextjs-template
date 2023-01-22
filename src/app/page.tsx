a632930eae9e0efc46bbfe383e1a7a91d3d0ee2b
Radhakrishna Sanka <rkrishnasanka@gmail.com>
2023-01-22 03:56:02 +0000

    Confirmed that tailwind is working in the code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,13 @@
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
-import styles from './page.module.css'
 import React from 'react'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <h1 className="text-5xl font-bold underline">
-      Hello world!
+    <h1 className="text-5xl font-medium animate-bounce text-gray-900 underline">
+      Hello world! xxx
     </h1>
   )
 }
