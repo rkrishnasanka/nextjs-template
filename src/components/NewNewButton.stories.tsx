2000c2cced7bdab9ff74d3c552b3165df1e39908
Radhakrishna Sanka <rkrishnasanka@gmail.com>
2023-01-22 05:25:42 +0000

    Got all the storybook adding a baseline test to work without breaking stuff.
    TODO: Make Tailwind work now

--- src/components/NewNewButton.stories.tsx
+++ src/components/NewNewButton.stories.tsx
@@ -9,33 +9,12 @@ export default {
   component: NewNewButton,
   // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
   argTypes: {
-    backgroundColor: { control: 'color' },
   },
 } as ComponentMeta<typeof NewNewButton>;
 
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
-const Template: ComponentStory<typeof NewNewButton> = (args) => <NewNewButton {...args} />;
+const Template: ComponentStory<typeof NewNewButton> = () => <NewNewButton />;
 
 export const Primary = Template.bind({});
-// More on args: https://storybook.js.org/docs/react/writing-stories/args
-Primary.args = {
-  primary: true,
-  label: 'NewButton',
-};
 
-export const Secondary = Template.bind({});
-Secondary.args = {
-  label: 'NewButton',
-};
 
-export const Large = Template.bind({});
-Large.args = {
-  size: 'large',
-  label: 'NewButton',
-};
-
-export const Small = Template.bind({});
-Small.args = {
-  size: 'small',
-  label: 'NewButton',
-};
